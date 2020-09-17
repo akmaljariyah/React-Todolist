a9d1a07b1731bae6e72388d410338d85d3c08622
akmaljariyah <64352294+akmaljariyah@users.noreply.github.com>
2020-09-17 16:16:24 +0700

    Create TodoForm with Hooks

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("No Blank!");
+      return;
+    }
+
+    addTodo(value);
+    alert(value);
+    setValue("");
+  };
+
+  console.log("value", value);
   return (
     <section className="add">
-      <form action="" className="add-form">
-        <input type="text" className="add-input" />
+      <form className="add-form" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          className="add-input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <button className="add-btn">Add</button>
       </form>
     </section>
   );
+};
+
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired
 };
 
 export default TodoForm;
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,32 +1,27 @@
 import React from "react";
+import Todo from "./Todo";
 
 const Todos = () => {
+  const todos = [
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar Props di React"
+    },
+    {
+      text: "Belajar React Hooks"
+    },
+    {
+      text: "Belajar State di React"
+    }
+  ];
+
   return (
     <section className="todos">
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
+      {todos.map((todo) => {
+        return <Todo text={todo.text} />;
+      })}
     </section>
   );
 };
