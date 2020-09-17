43a079405d20d1a96e51f3a1fdb7ce1f4cd7198d
akmaljariyah <64352294+akmaljariyah@users.noreply.github.com>
2020-09-17 16:26:10 +0700

    Refactor Todos and TodoList Component

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Paper from "../components/Paper";
 import Header from "../components/Header";
 import TodoForm from "../components/TodoForm";
 import Todos from "../components/Todos";
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { text: "Belajar React" },
+    { text: "Belajar Props di React" },
+    { text: "Belajar React Hooks" },
+    { text: "Belajar State di React" }
+  ]);
+
   return (
     <Paper>
       <Header />
       <TodoForm />
-      <Todos />
+      <Todos todos={todos} />
     </Paper>
   );
 };
